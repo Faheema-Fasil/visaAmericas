d225b6f2730da0da3792bd443ca5d23e3cbe34ef
Amaldev <amaldev1845@gmail.com>
2025-06-12 12:00:23 +0530

    d1

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,14 +6,18 @@ function HomePage() {
     <>
       <div className="container">
         <div
-          className="bg-cover bg-center h-screen w-full"
+          className="bg-cover bg-center w-full"
           style={{ backgroundImage: `url(${bg1})` }}
         >
-          <div className="flex items-center justify-center h-full bg-black/50">
-            <h1 className="text-white text-4xl font-bold">Welcome</h1>
+          <div
+            className="bg-gradient-to-b from-[#073D40] via-[#073D40] to-[#397C7A] bg-opacity-90 transition-all duration-300 rounded-md"
+          >
+            <div className="flex items-center justify-center bg-black/50">
+              <h1 className="text-white text-4xl font-bold h-full">Welcomehbfhfghfh</h1>
+            </div>
           </div>
         </div>
-      </div>
+      </div >
 
     </>
   )
