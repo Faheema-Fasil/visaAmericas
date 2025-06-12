d67e37b040eeb154e556ed23179325f393a74cae
Amaldev <amaldev1845@gmail.com>
2025-06-12 12:06:59 +0530

    component folder created

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -10,10 +10,10 @@ function HomePage() {
           style={{ backgroundImage: `url(${bg1})` }}
         >
           <div
-            className="bg-gradient-to-b from-[#073D40] via-[#073D40] to-[#397C7A] bg-opacity-90 transition-all duration-300 rounded-md"
+            className="bg-gradient-to-b from-[#073D40] via-[#073D40] to-[#397C7A] bg-opacity-50 transition-all duration-300 rounded-md"
           >
             <div className="flex items-center justify-center bg-black/50">
-              <h1 className="text-white text-4xl font-bold h-full">Welcomehbfhfghfh</h1>
+              <h1 className="text-white text-4xl font-bold h-full">Welcomehbfhfghfh Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam doloremque dolorem odit quaerat nesciunt nisi facere tenetur quis asperiores, quasi odio soluta blanditiis magnam provident veniam natus ea tempore assumenda, repellendus incidunt saepe voluptatibus nobis! Eius tempore sapiente necessitatibus aliquam officiis totam praesentium quis minima accusantium, temporibus iure labore neque qui sit harum cupiditate repellat placeat eum quasi. Ut soluta temporibus illum vero provident saepe unde nulla debitis, at consequuntur voluptates nemo, dolor nisi maiores veniam, est nam maxime quis neque dicta accusamus delectus. Facilis quaerat tempore cumque officiis necessitatibus fugiat, animi sint dolorum suscipit rem consectetur dolore, aliquid reiciendis.</h1>
             </div>
           </div>
         </div>
